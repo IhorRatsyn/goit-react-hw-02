20787a407f2a2984c35d1cf6f0485717f220bf30
IhorRatsyn <ratsinjob@gmail.com>
2024-02-10 18:40:54 +0200

    Descrioptions -> Descrioption

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,57 @@
-import {useState} from 'react'
-import './App.css'
-import Descriptions from "./components/Descriptions.jsx";
+import { useState } from "react";
+import "./App.css";
+import Description from "./components/Description.jsx";
 import Options from "./components/Options.jsx";
 import Feedback from "./components/Feedback.jsx";
 
-const localStorageKey = 'options'
+const localStorageKey = "options";
 
 const defaultState = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-}
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
-const getStorage = () => JSON.parse(localStorage.getItem(localStorageKey)) || defaultState
-const setStorage = (val) => localStorage.setItem(localStorageKey, JSON.stringify(val))
+const getStorage = () =>
+  JSON.parse(localStorage.getItem(localStorageKey)) || defaultState;
+const setStorage = (val) =>
+  localStorage.setItem(localStorageKey, JSON.stringify(val));
 
 function App() {
-    const title = 'Sip Happens Café'
-    const desc = 'Please leave your feedback about our service by selecting one of the options below.'
+  const title = "Sip Happens Café";
+  const desc =
+    "Please leave your feedback about our service by selecting one of the options below.";
 
-    const [options, setOptions] = useState(getStorage)
+  const [options, setOptions] = useState(getStorage);
 
-    const onOptionsClick = (type) => {
-        const newOptions = {...options}
-        newOptions[type] += 1
-        setOptions(newOptions)
-        setStorage(newOptions)
-    }
+  const onOptionsClick = (type) => {
+    const newOptions = { ...options };
+    newOptions[type] += 1;
+    setOptions(newOptions);
+    setStorage(newOptions);
+  };
 
-    const onReset = () => {
-        setOptions(defaultState)
-    }
+  const onReset = () => {
+    setOptions(defaultState);
+  };
 
-    return (
-        <>
-            <div>
-                <Descriptions title={title} desc={desc}/>
-                <Options options={Object.keys(options)} handlerClick={onOptionsClick} onReset={onReset}/>
-                <Feedback good={options.good} bad={options.bad} neutral={options.neutral}/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Description title={title} desc={desc} />
+        <Options
+          options={Object.keys(options)}
+          handlerClick={onOptionsClick}
+          onReset={onReset}
+        />
+        <Feedback
+          good={options.good}
+          bad={options.bad}
+          neutral={options.neutral}
+        />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
