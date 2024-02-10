2bcb6c7b9633c883931e4c7242089d488859ceae
IhorRatsyn <ratsinjob@gmail.com>
2024-02-11 01:04:37 +0200

    Initual state corection + deletion by ID

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,39 +1,40 @@
-import React, {useMemo, useState} from 'react';
-import PropTypes from 'prop-types';
+import React, { useMemo, useState } from "react";
+import PropTypes from "prop-types";
 import options from "./Options.jsx";
 
-const Feedback = ({neutral, good, bad}) => {
-    const [totalFeedback, setTotalFeedback] = useState(0)
-    const [positiveFeedback, setPositiveFeedback] = useState(0)
+const Feedback = ({ neutral, good, bad }) => {
+  const [totalFeedback, setTotalFeedback] = useState(0);
+  const [positiveFeedback, setPositiveFeedback] = useState(0);
 
-    useMemo(() => {
-        const allSum = good + neutral + bad
-        setTotalFeedback(allSum)
+  useMemo(() => {
+    const allSum = good + neutral + bad;
+    setTotalFeedback(allSum);
 
-        const positivePercent = Math.round(((good + neutral) / allSum) * 100)
-        setPositiveFeedback(allSum  ? positivePercent : 0)
-    }, [neutral, good, bad])
+    const positivePercent = Math.round(((good + neutral) / allSum) * 100);
+    setPositiveFeedback(allSum ? positivePercent : 0);
+  }, [neutral, good, bad]);
 
-    return <div className='options'>
-        {
-            totalFeedback
-                ?
-                    <div className='feedback-setting'>
-                      <div>Good: {good}</div>
-                      <div>Bad: {bad}</div>
-                      <div>Neutral: {neutral}</div>
-                      <div>Total: {totalFeedback}</div>
-                      <div>Positive: {positiveFeedback}%</div>
-                    </div>
-                : <div>No feedback yet</div>
-        }
+  return (
+    <div className="options">
+      {totalFeedback ? (
+        <div className="feedback-setting">
+          <div>Good: {good}</div>
+          <div>Bad: {bad}</div>
+          <div>Neutral: {neutral}</div>
+          <div>Total: {totalFeedback}</div>
+          <div>Positive: {positiveFeedback}%</div>
+        </div>
+      ) : (
+        <div>No feedback yet</div>
+      )}
     </div>
+  );
 };
 
 Feedback.propTypes = {
-    good: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
 };
 
 export default Feedback;
